dbaf9f9f12d6571919d876757c1a8dd8f5c6f288
aayush goyal <aayushgoyal9507@gmail.com>
2019-06-12 00:03:12 -0600

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,5 @@
 console.log('Client side javascript file loaded')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
-
-// fetch('http://localhost:3000/weather?address=!').then(response => {
-//     response.json().then((data) => {
-//         if (data.error){
-//             console.log(data.error)
-//         }
-//         else {
-//             console.log(data.location)
-//             console.log(data.forecast)
-//         }
-//     })
-// })
-
 const weatherform = document.querySelector('form')
 const search = document.querySelector('input')
 
@@ -30,15 +12,13 @@ weatherform.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
 
-    // console.log(location)
 
-    fetch('http://localhost:3000/weather?address='+location).then(response => {
+    fetch('/weather?address='+location).then(response => {
     response.json().then((data) => {
         if (data.error){
             msg1.textContent = data.error
         }
         else {
-            // console.log(data.location)
             msg1.textContent = data.location
             msg1.textContent = data.forecast
 
--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast')
 
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicdirPath = path.join(__dirname,'../public')
@@ -106,7 +107,7 @@ app.get('*',(req,res) => {
 })
 
 
-app.listen(3000,() => {
+app.listen(port,() => {
     console.log('Server is up  on port 3000')
 })
 
